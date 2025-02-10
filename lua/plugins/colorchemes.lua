7e2f4c0d54412ff308e20f737bf0eeeaba723a26
DoanCongQui <congt4644@gmail.com>
2025-02-10 18:41:52 +0700

    Add lua vim

---
+++ lua/plugins/colorchemes.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = true,
+      }
+    end,
+  },
+  {
+    "Mofiqul/vscode.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = true,
+      }
+    end,
+  },
+}
